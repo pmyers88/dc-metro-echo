2271484fdc88ab5c1947bf56c213dfeaa0fb39f9
pmyers88 <prm5031@gmail.com>
2015-05-08 00:42:13 -0400

    Add LaunchRequest test

--- test/controllers.js
+++ test/controllers.js
@@ -36,7 +36,38 @@ describe('Controllers', function() {
     });
     describe('POST /v1/echo_request', function() {
       describe('LaunchRequest', function() {
-        it('should welcome the user to the app');
+        it('should welcome the user to the app', function(done) {
+          request(app)
+            .post('/v1/echo_request')
+            .set('Host', 'api.example.com')
+            .send({
+              version: '1.0',
+              session: {
+                new: true,
+                sessionId: 'amznSessionId',
+                attributes: {},
+                user: {
+                  userId: 'amznUserId'
+                }
+              },
+              request: {
+                type: 'LaunchRequest',
+                requestId: 'amznReqId'
+              }
+            })
+            .expect(200)
+            .end(function(err, res) {
+              if (err) throw err;
+              res.body.version.should.equal('1.0');
+              res.body.response.outputSpeech.type.should.equal('PlainText');
+              res.body.response.outputSpeech.text.should.equal('Welcome to the DC Metro App! How can I help you?');
+              res.body.response.card.type.should.equal('Simple');
+              res.body.response.card.title.should.equal('DC Metro Echo');
+              res.body.response.card.subtitle.should.equal('Metro App');
+              res.body.response.shouldEndSession.should.equal(false);
+              done();
+            });
+        });
       });
       describe('IntentRequest', function() {
         describe('GetMetroTimes', function() {
