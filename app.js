bb6c8557f04c10a747baf104d27d992953ddf86b
pmyers88 <prm5031@gmail.com>
2015-05-10 17:28:29 -0400

    Implement GetDestinationStation request Fixes #7

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var app = express();
+var fs = require('fs');
+var path = require('path');
+var morgan = require('morgan');
+var fileStreamRotator = require('file-stream-rotator');
 var bodyParser = require('body-parser');
+var express = require('express');
+
 var controllers = require('./controllers');
 
+var app = express();
+var logDirectory = path.join(__dirname, '/log');
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+var accessLogStream = fileStreamRotator.getStream({
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false
+});
+
+app.use(morgan('combined', { stream: accessLogStream }));
 app.use(bodyParser.json());
 app.use(controllers);
 
---
+++ helpers/utils.js
@@ -0,0 +1,38 @@
+var _ = require('lodash');
+var correction = require('../resources/station_corrections');
+var abbreviation = require('../resources/station_abbreviations');
+
+var changeTypeLookup = {
+  correction: correction,
+  abbreviation: abbreviation
+};
+
+var isNullOrUndefined = function(val) {
+  return _.isNull(val) || _.isUndefined(val);
+};
+
+var changeStationName = function(stationName, changeType) {
+  var mapping = changeTypeLookup[changeType];
+  return _.has(mapping, stationName) ?
+    mapping[stationName] :
+    stationName;
+};
+
+var joinListConjuction = function(list, separator, conjuction) {
+  var joinedList = '';
+  if (list.length === 1) {
+    joinedList = list[0];
+  } else {
+    joinedList = list.slice(0, list.length - 1).join(separator) +
+      (list.length > 1 ?
+        conjuction + _.last(list) :
+        '');
+  }
+  return joinedList;
+};
+
+module.exports = {
+  isNullOrUndefined: isNullOrUndefined,
+  changeStationName: changeStationName,
+  joinListConjuction: joinListConjuction
+};
