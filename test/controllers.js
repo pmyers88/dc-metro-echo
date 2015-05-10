8e74a7788058b655a1efa979ec7ba40075d3e26a
pmyers88 <prm5031@gmail.com>
2015-05-09 23:54:25 -0400

    IntentRequest test

--- test/controllers.js
+++ test/controllers.js
@@ -64,6 +64,7 @@ describe('Controllers', function() {
               res.body.response.card.type.should.equal('Simple');
               res.body.response.card.title.should.equal('DC Metro Echo');
               res.body.response.card.subtitle.should.equal('Metro App');
+              res.body.response.card.content.should.equal('Welcome to the DC Metro App! How can I help you?');
               res.body.response.shouldEndSession.should.equal(false);
               done();
             });
@@ -71,7 +72,48 @@ describe('Controllers', function() {
       });
       describe('IntentRequest', function() {
         describe('GetMetroTimes', function() {
-          it('should return metro times');
+          it('should return metro times', function(done) {
+          request(app)
+            .post('/v1/echo_request')
+            .set('Host', 'api.example.com')
+            .send({
+              version: '1.0',
+              session: {
+                new: true,
+                sessionId: 'amznSessionId',
+                attributes: {},
+                user: {
+                  userId: 'amznUserId'
+                }
+              },
+              request: {
+                type: 'IntentRequest',
+                requestId: 'amznReqId',
+                intent: {
+                  name: 'GetMetroTimes',
+                  slots: {
+                    station: {
+                      name: 'station',
+                      value: 'ballston'
+                    }
+                  }
+                }
+              }
+            })
+            .expect(200)
+            .end(function(err, res) {
+              if (err) throw err;
+              res.body.version.should.equal('1.0');
+              res.body.response.outputSpeech.type.should.equal('PlainText');
+              res.body.response.outputSpeech.text.should.match(/^(The next train to [^ ]+ leaves in \d+ minutes.)+/);
+              res.body.response.card.type.should.equal('Simple');
+              res.body.response.card.title.should.equal('Train Arrivals');
+              res.body.response.card.subtitle.should.equal('Here are the train arrivals');
+              res.body.response.card.content.should.match(/^(The next train to [^ ]+ leaves in \d+ minutes.)+/);
+              res.body.response.shouldEndSession.should.equal(true);
+              done();
+            });
+        });
         });
       });
       describe('SessionEndedRequest', function() {
