f652b1f609072cc8306369228b5c80ff50b01641
pmyers88 <prm5031@gmail.com>
2015-05-15 01:15:43 -0400

    Enable SSL

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var morgan = require('morgan');
 var fileStreamRotator = require('file-stream-rotator');
 var bodyParser = require('body-parser');
 var express = require('express');
+var https = require('https');
+var http = require('http');
 
 var controllers = require('./controllers');
 
@@ -22,10 +24,13 @@ app.use(morgan('combined', { stream: accessLogStream }));
 app.use(bodyParser.json());
 app.use(controllers);
 
-app.set('port', (process.env.PORT || 5000));
+var https_options = {
+  key: fs.readFileSync('/etc/ssl/certs/server.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/server.crt'),
+  ca: fs.readFileSync('/etc/ssl/certs/ca.crt')
+};
 
-var server = app.listen(app.get('port'), function() {
-  console.log('App listening on port', app.get('port'));
-});
+http.createServer(app).listen(process.env.PORT || 5000);
+https.createServer(https_options, app).listen(process.env.SECURE_PORT || 4443);
 
 module.exports.app = app;
