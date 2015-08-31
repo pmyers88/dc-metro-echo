5520a62b1935c213d19826cf1cb834f6d83e3969
Phil Myers <prm5031@gmail.com>
2015-08-30 22:19:28 -0400

    Refactored code to be more modular

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/api/v1/echo_request', require('./echo_request'));
+router.use('/api/v1/echo_request', require('./metro_transit_api'));
 
 router.get('/', function(req, res) {
   res.send('DC Metro App is up and ready to go!!!');
