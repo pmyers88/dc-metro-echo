71083a50a6cc900edb272dded2c3578ee5065d2d
RJ Beers <r33beers@gmail.com>
2016-01-24 15:54:13 -0500

    Update driver file for new module interfaces

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-require('./lib/metro-transit')
+var MetroTransit = require('./lib/metro-transit');
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the MetroTransit skill.
-    var dcMetro = new MetroTransit();
-    dcMetro.execute(event, context);
+    MetroTransit.execute(event, context);
 };
