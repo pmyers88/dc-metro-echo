edccb900451365e210399ea7dca89fb5a21fb008
Phil Myers <prm5031@gmail.com>
2016-02-07 16:22:20 -0500

    Add semistandard linter
    Create task for testing and linting; fix linter errors
    
    Fix errors found by linter
    
    Move AlexaSkill to vendor since it's not code that we wrote
    
    Update README

--- gulpfile.js
+++ gulpfile.js
@@ -8,58 +8,71 @@ var fs = require('fs');
 var runSequence = require('run-sequence');
 var tape = require('gulp-tape');
 var faucet = require('faucet');
+var semistandard = require('gulp-semistandard');
 
-gulp.task('test', function() {
+gulp.task('test', function () {
   return gulp.src('test/*.js')
     .pipe(tape({
       reporter: faucet()
     }));
 });
 
-gulp.task('clean', function() {
+gulp.task('semistandard', function () {
+  return gulp.src(['*.js', 'lib/*.js'])
+    .pipe(semistandard())
+    .pipe(semistandard.reporter('default', {
+      breakOnError: true
+    }));
+});
+
+gulp.task('clean', function () {
   return del('./dist');
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
   return gulp.src(['index.js', '.env'])
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('lib', function() {
+gulp.task('lib', function () {
   return gulp.src('./lib/*')
     .pipe(gulp.dest('./dist/lib/'));
 });
 
-gulp.task('resources', function() {
+gulp.task('resources', function () {
   return gulp.src('./resources/*')
     .pipe(gulp.dest('./dist/resources/'));
 });
 
+gulp.task('vendor', function () {
+  return gulp.src('./vendor/*')
+    .pipe(gulp.dest('./dist/vendor/'));
+});
+
 // Here we want to install npm packages to dist, ignoring devDependencies.
-gulp.task('npm', function() {
+gulp.task('npm', function () {
   return gulp.src('./package.json')
     .pipe(gulp.dest('./dist/'))
     .pipe(install({production: true}));
 });
 
 // Now the dist directory is ready to go. Zip it.
-gulp.task('zip', function() {
+gulp.task('zip', function () {
   return gulp.src(['dist/**/*', '!dist/package.json', 'dist/.*'])
     .pipe(zip('dc-metro-echo.zip'))
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('upload', function() {
-
+gulp.task('upload', function () {
   AWS.config.region = 'us-east-1';
   var lambda = new AWS.Lambda();
   var functionName = 'MetroTransit';
 
-  lambda.getFunction({FunctionName: functionName}, function(err, data) {
+  lambda.getFunction({FunctionName: functionName}, function (err, data) {
     if (err) {
       var warning;
       if (err.statusCode === 404) {
-        warning = 'Unable to find lambda function ' + deploy_function + '. ';
+        warning = 'Unable to find lambda function ' + functionName + '. ';
         warning += 'Verify the lambda function name and AWS region are correct.';
         gutil.log(warning);
       } else {
@@ -69,33 +82,45 @@ gulp.task('upload', function() {
       }
     }
 
-    var current = data.Configuration;
     var params = {
       FunctionName: functionName
     };
 
-    fs.readFile('./dc-metro-echo.zip', function(err, data) {
-      params.ZipFile = data;
-      lambda.updateFunctionCode(params, function(err, data) {
-        if (err) {
-          console.log(JSON.stringify(err));
-          var warning = 'Package upload failed. ';
-          warning += 'Check your iam:PassRole permissions.';
-          gutil.log(warning);
-        }
-      });
+    fs.readFile('./dc-metro-echo.zip', function (err, data) {
+      if (err) {
+        console.log(JSON.stringify(err));
+        var warning = 'Error creating zip.';
+        gutil.log(warning);
+      } else {
+        params.ZipFile = data;
+        lambda.updateFunctionCode(params, function (err, data) {
+          if (err) {
+            console.log(JSON.stringify(err));
+            var warning = 'Package upload failed. ';
+            warning += 'Check your iam:PassRole permissions.';
+            gutil.log(warning);
+          }
+        });
+      }
     });
   });
 });
 
-
-gulp.task('default', function(callback) {
+gulp.task('default', function (callback) {
   return runSequence(
+    ['semistandard'],
     ['test'],
     ['clean'],
-    ['js', 'lib', 'resources', 'npm'],
+    ['js', 'lib', 'vendor', 'resources', 'npm'],
     ['zip'],
     ['upload'],
     callback
+  );
+});
+
+gulp.task('lintTest', function (callback) {
+  return runSequence(
+    ['semistandard'],
+    ['test']
   );
 });
--- test/test-utils.js
+++ test/test-utils.js
@@ -1,47 +1,47 @@
 'use strict';
 
-var test = require("tape");
-var utils = require("../lib/utils")
+var test = require('tape');
+var utils = require('../lib/utils');
 
-test("test isNullOrUndefined logic", function(t) {
+test('test isNullOrUndefined logic', function (t) {
   t.plan(5);
 
   t.ok(utils.isNullOrUndefined(null));
   t.ok(utils.isNullOrUndefined(undefined));
   t.notOk(utils.isNullOrUndefined(3));
-  t.notOk(utils.isNullOrUndefined("null"));
-  t.notOk(utils.isNullOrUndefined("undefined"));
+  t.notOk(utils.isNullOrUndefined('null'));
+  t.notOk(utils.isNullOrUndefined('undefined'));
 });
 
-test("test changeStationName with corrections", function(t) {
+test('test changeStationName with corrections', function (t) {
   t.plan(3);
 
-  t.equal(utils.changeStationName("boston", "correction"), "ballston");
-  t.equal(utils.changeStationName("ballston", "correction"), "ballston");
-  t.equal(utils.changeStationName("not listed", "correction"), "not listed");
+  t.equal(utils.changeStationName('boston', 'correction'), 'ballston');
+  t.equal(utils.changeStationName('ballston', 'correction'), 'ballston');
+  t.equal(utils.changeStationName('not listed', 'correction'), 'not listed');
 });
 
-test("test changeStationName with abbreviations", function(t) {
+test('test changeStationName with abbreviations', function (t) {
   t.plan(3);
 
-  t.equal(utils.changeStationName("Achives-Navy Memorial-Penn Quarter", "abbreviation"), "Archives");
-  t.equal(utils.changeStationName("Wiehle Reston East", "abbreviation"), "Wiehle Reston East");
-  t.equal(utils.changeStationName("not listed", "abbreviation"), "not listed");
+  t.equal(utils.changeStationName('Achives-Navy Memorial-Penn Quarter', 'abbreviation'), 'Archives');
+  t.equal(utils.changeStationName('Wiehle Reston East', 'abbreviation'), 'Wiehle Reston East');
+  t.equal(utils.changeStationName('not listed', 'abbreviation'), 'not listed');
 });
 
-test("joinListConjuction test", function(t) {
+test('joinListConjuction test', function (t) {
   t.plan(3);
 
   let things = [
-    "this",
-    "that",
-    "other"
+    'this',
+    'that',
+    'other'
   ];
   let thing = [
-    "this"
+    'this'
   ];
 
-  t.equal(utils.joinListConjuction(things, ", ", " or "), "this, that or other");
-  t.equal(utils.joinListConjuction(things, ", ", " and "), "this, that and other");
-  t.equal(utils.joinListConjuction(thing, ", ", " and "), "this");
+  t.equal(utils.joinListConjuction(things, ', ', ' or '), 'this, that or other');
+  t.equal(utils.joinListConjuction(things, ', ', ' and '), 'this, that and other');
+  t.equal(utils.joinListConjuction(thing, ', ', ' and '), 'this');
 });
