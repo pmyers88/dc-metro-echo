65976dd57f7bbebc78643e519f65fb0f4f9c8aca
Phil Myers <prm5031@gmail.com>
2016-02-14 12:39:31 -0500

    Test incidents endpoint

--- test/test-wmata-api.js
+++ test/test-wmata-api.js
@@ -9,22 +9,33 @@ var test = tapeNock(tape, {
   fixtures: path.join(__dirname, 'fixtures')
 });
 
-test('wmata api get valid key', function (t) {
-  var url = props.stationArrivalWmataUrl + 'B02?api_key=' + process.env.WMATA_API_KEY;
+test('wmata api get station prediction valid key', function (t) {
+  var url = props.stationArrivalWmataUrl + 'B02';
   wmataApi.get(url, function (response) {
     t.plan(2);
     t.ok(response);
-    t.expect(response.Trains[0]).to.have.keys('Car', 'Destination', 'DestinationCode', 'DestinationName', 'Group', 'Line', 'LocationCode', 'LocationName', 'Min');
+    t.expect(response.Trains[0]).to.have.keys('Car', 'Destination', 'DestinationCode', 'DestinationName', 'Group',
+      'Line', 'LocationCode', 'LocationName', 'Min');
     t.end();
   });
 });
 
-test('wmata api get invalid key', function (t) {
-  var url = props.stationArrivalWmataUrl + 'FooBar?api_key=' + process.env.WMATA_API_KEY;
-  wmataApi.get(url, function (response) {}, function (error) {
+test('wmata api get station prediction invalid key', function (t) {
+  var url = props.stationArrivalWmataUrl + 'FooBar';
+  wmataApi.get(url, null, function (error) {
     t.plan(2);
     t.expect(error.statusCode).to.be(400);
     t.expect(JSON.parse(error.body).Message).to.be('Station Code(s) not specified, invalid, or does not exist.');
+    t.end();
+  });
+});
+
+test('wmata api get incidents', function (t) {
+  wmataApi.get(props.serviceAdvisoriesWmataUrl, function (response) {
+    t.plan(2);
+    t.ok(response);
+    t.expect(response.Incidents[0]).to.have.keys('IncidentID', 'Description', 'StartLocationFullName',
+      'EndLocationFullName', 'PassengerDelay', 'DelaySeverity', 'IncidentType', 'EmergencyText', 'LinesAffected', 'DateUpdated');
     t.end();
   });
 });
