88a6ffc175606b02d4e98cdb690f8e5bfc1fa9fc
Phil Myers <prm5031@gmail.com>
2016-03-23 22:58:52 -0400

    Better voice interface when there is one station option

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,5 +1,6 @@
 var test = require('tape');
 var utils = require('../lib/utils');
+var props = require('../resources/properties');
 
 test('test isNullOrUndefined logic', function (t) {
   t.plan(5);
@@ -42,4 +43,24 @@ test('joinListConjuction test', function (t) {
   t.equal(utils.joinListConjuction(things, ', ', ' or '), 'this, that or other');
   t.equal(utils.joinListConjuction(things, ', ', ' and '), 'this, that and other');
   t.equal(utils.joinListConjuction(thing, ', ', ' and '), 'this');
+});
+
+test('makeGetStationResponseText tests', function (t) {
+  t.plan(3);
+  var multipleStationArrivals = {
+    'largo town center': ['4', '8'],
+    'vienna': ['12', '16'],
+    'wiehle reston east': ['9'],
+    'new carrollton': ['2']
+  };
+  var oneStationArrival = {
+    'vienna': ['7']
+  };
+  var stationName = 'ballston';
+
+  t.equal(utils.makeGetStationResponseText(multipleStationArrivals, stationName), 'Are you going to largo town ' +
+    'center, vienna, wiehle reston east or new carrollton');
+  t.equal(utils.makeGetStationResponseText(oneStationArrival, stationName), 'The next train to vienna arrives in 7 ' +
+    'minutes. There are no other trains arriving at ballston in the next 20 minutes.');
+  t.equal(utils.makeGetStationResponseText({}, stationName), props.stationNoArrivalsSpeechOutput);
 });
