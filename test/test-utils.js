9eb2708cf0dd00562960c2a6a5751cdf6fbcc047
Phil Myers <prm5031@gmail.com>
2016-03-25 00:02:31 -0400

    Abbreviate boarding/arriving; make abbreviations more general

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,5 +1,6 @@
 var test = require('tape');
 var utils = require('../lib/utils');
+var abbreviations = require('../resources/abbreviations');
 
 test('test isNullOrUndefined logic', function (t) {
   t.plan(5);
@@ -27,7 +28,7 @@ test('test changeStationName with abbreviations', function (t) {
   t.equal(utils.changeStationName('not listed', 'abbreviation'), 'not listed');
 });
 
-test('joinListConjuction test', function (t) {
+test('joinListConjunction test', function (t) {
   t.plan(3);
 
   var things = [
@@ -39,9 +40,9 @@ test('joinListConjuction test', function (t) {
     'this'
   ];
 
-  t.equal(utils.joinListConjuction(things, ', ', ' or '), 'this, that or other');
-  t.equal(utils.joinListConjuction(things, ', ', ' and '), 'this, that and other');
-  t.equal(utils.joinListConjuction(thing, ', ', ' and '), 'this');
+  t.equal(utils.joinListConjunction(things, ', ', ' or '), 'this, that or other');
+  t.equal(utils.joinListConjunction(things, ', ', ' and '), 'this, that and other');
+  t.equal(utils.joinListConjunction(thing, ', ', ' and '), 'this');
 });
 
 test('test makeGetStationResponseText for zero, single or multiple stations', function (t) {
@@ -64,13 +65,40 @@ test('test makeGetStationResponseText for zero, single or multiple stations', fu
   t.equal(utils.makeGetStationResponseText({}, stationName), 'Sorry, there are no trains running at this time.');
 });
 
+test('test makeGetDestinationResponseText for zero, single or multiple stations', function (t) {
+  t.plan(6);
+  var multIntArrivals = ['4', '8'];
+  var singleIntArrival = ['5'];
+  var singleBrdSingleIntArrivals = ['BRD', '6'];
+  var singleBrdMultIntArrivals = ['BRD', '6', '8'];
+  var multBrdSingleIntArrivals = ['BRD', 'ARR', '6'];
+  var singleBrdArrival = ['BRD'];
+
+  var stationName = 'ballston';
+
+  t.equal(utils.makeGetDestinationResponseText(multIntArrivals, stationName), 'The next 2 trains to ballston arrive ' +
+    'in 4 and 8 minutes.');
+  t.equal(utils.makeGetDestinationResponseText(singleIntArrival, stationName), 'The next train to ballston arrives ' +
+    'in 5 minutes.');
+  t.equal(utils.makeGetDestinationResponseText(singleBrdSingleIntArrivals, stationName), 'The next train to ballston is ' +
+    'boarding now. There is also a train arriving in 6 minutes.');
+  t.equal(utils.makeGetDestinationResponseText(singleBrdMultIntArrivals, stationName), 'The next train to ballston is ' +
+    'boarding now. There are also trains arriving in 6 and 8 minutes.');
+  t.equal(utils.makeGetDestinationResponseText(multBrdSingleIntArrivals, stationName), 'The next train to ballston is ' +
+    'boarding now. There is also a train arriving in 6 minutes.');
+  t.equal(utils.makeGetDestinationResponseText(singleBrdArrival, stationName), 'The next train to ballston is ' +
+    'boarding now.');
+});
+
 test('test sanitizeServiceAdvisories replaces known abbreviations with words', function (t) {
-  t.plan(1);
+  t.plan(2);
   var serviceAdvisoryText = 'Blu/Org Line: Single tracking btwn Stadium-Armory & Eastern Market due to scheduled ' +
     'track work. Expect delays through tonight\'s closing. Silver Line: Trains operating btwn Wiehle-Reston & ' +
     'Ballston only due to scheduled track work. Use Orange/Blue Lines to/from other stations.';
-  t.equal(utils.sanitizeServiceAdvisories(serviceAdvisoryText), 'Blue/Orange Line: Single tracking between ' +
+  t.equal(utils.replaceAbbreviations(serviceAdvisoryText, abbreviations['advisories']), 'Blue/Orange Line: Single tracking between ' +
     'Stadium-Armory & Eastern Market due to scheduled track work. Expect delays through tonight\'s closing. Silver ' +
       'Line: Trains operating between Wiehle-Reston & Ballston only due to scheduled track work. Use Orange/Blue ' +
       'Lines to/from other stations.');
+  var arrivalsText = 'The next train to vienna is BRD now.';
+  t.equal(utils.replaceAbbreviations(arrivalsText, abbreviations['arrivals']), 'The next train to vienna is boarding now.');
 });
