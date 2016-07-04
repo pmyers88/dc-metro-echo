95d808476abd7a7f595a6dc19c81243ffb73bc0a
Phil Myers <prm5031@gmail.com>
2016-07-03 22:06:58 -0400

    Finish refactor

--- test/test-metro-transit.js
+++ test/test-metro-transit.js
@@ -1,7 +1,7 @@
-var test = require('tape');
-var MetroTransit = require('../lib/metro-transit.js');
+const test = require('tape');
+const MetroTransit = require('../lib/metro-transit.js');
 
-test('MetroTransit eventHandlers has the right methods', function (t) {
+test('MetroTransit eventHandlers has the right methods', (t) => {
   t.plan(4);
 
   t.equal(typeof MetroTransit.eventHandlers.onIntent, 'function', 'onIntent method exists');
@@ -10,7 +10,7 @@ test('MetroTransit eventHandlers has the right methods', function (t) {
   t.equal(typeof MetroTransit.eventHandlers.onSessionStarted, 'function', 'onSessionStarted method exists');
 });
 
-test('MetroTransit intentHandlers has the right methods', function (t) {
+test('MetroTransit intentHandlers has the right methods', (t) => {
   t.plan(6);
 
   t.equal(typeof MetroTransit.intentHandlers['AMAZON.HelpIntent'], 'function', 'AMAZON.HelpIntent method exists');
